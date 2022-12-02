395a7f8a77e144cde11740af5417cc9b0d1bf377
Yasna <y.noushirvani@gmail.com>
2022-12-02 18:00:04 +0330

    Modified hello.c and hello.exe
    This is the second change on branch 2.

--- hello.c
+++ hello.c
@@ -4,6 +4,7 @@ int main()
 {
     printf("Hello!\n");
     printf("Welcome to my first git repository.");
-    printf("\nThis change was made in branch2.");
+    printf("\nThis change was made on branch2.");
+    printf("\nStill on branch 2!");
     return 0;
 }
